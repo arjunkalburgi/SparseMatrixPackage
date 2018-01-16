11c16a3ef2f0c70504acdad6f193b29829ba09c5
Andrew McKernan <amckerna@ualberta.ca>
2018-01-16 16:37:29 -0700

    added file for sparse matrix, no code besides method names

---
+++ sparse_matrix.rb
@@ -0,0 +1,72 @@
+# definition for the sparse matrix class
+
+
+class SparseMatrix
+	
+	def initialize()
+		#Pre 
+		
+		#Post
+		
+	end
+	
+	
+	def add(matrix1, matrix2)
+		#Pre 
+		
+		#Post
+		
+	end
+	
+	def subtract(matrix1, matrix2)
+		#Pre 
+		
+		#Post
+		
+	end
+	
+	def multiply(matrix1, matrix2)
+		#Pre 
+		
+		#Post
+		
+	end
+	
+	def divide(matrix1, matrix2)
+		#Pre 
+		
+		#Post
+		
+	end
+	
+	def exponent(matrix1, matrix2)
+		#Pre 
+		
+		#Post
+		
+	end
+	
+	def getDeterminant()
+		#Pre 
+		
+		#Post
+		
+	end
+	
+	def getInverse()
+		#Pre 
+		
+		#Post
+		
+	end
+	
+	def getTranspose()
+		#Pre 
+		
+		#Post
+		
+	end
+	
+	
+end
+
