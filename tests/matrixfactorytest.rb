2b0444d09ee14ebfb6b5f9ee19510c60dbc49b52
Arjun Kalburgi <askalburgi@gmail.com>
2018-01-16 16:48:45 -0700

    test files

---
+++ tests/matrixfactorytest.rb
@@ -0,0 +1,16 @@
+require 'test/unit'
+require_relative '../sparsematrixpackage/matrixfactory.rb'
+
+class matrixfactorytest < Test::Unit::TestCase
+    def setup
+        pass 
+    end 
+
+    def teardown
+        pass
+    end
+
+    def test_fail
+        assert(false, 'Assertion was false.')
+    end
+end
---
+++ tests/sparsematrixtest.rb
@@ -0,0 +1,16 @@
+require 'test/unit'
+require_relative '../sparsematrixpackage/sparsematrix.rb'
+
+class sparsematrixtest < Test::Unit::TestCase
+    def setup
+        pass 
+    end 
+
+    def teardown
+        pass
+    end
+
+    def test_fail
+        assert(false, 'Assertion was false.')
+    end
+end
