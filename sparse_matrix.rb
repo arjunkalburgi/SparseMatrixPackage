841cadd2c893c225325a06cc490b7850db2e8bbf
Andrew McKernan <amckerna@ualberta.ca>
2018-01-16 16:47:13 -0700

    changed a few function args, added a few comments about post conditions

--- sparse_matrix.rb
+++ sparse_matrix.rb
@@ -11,60 +11,60 @@ class SparseMatrix
 	end
 	
 	
-	def add(matrix1, matrix2)
+	def add(matrix)
 		#Pre 
 		
 		#Post
-		
+		# this sparse matrix has had the contents of give matrix added to it
 	end
 	
-	def subtract(matrix1, matrix2)
+	def subtract(matrix)
 		#Pre 
 		
 		#Post
-		
+		# this sparse matrix has had the contents of given matrix subtracted from it
 	end
 	
-	def multiply(matrix1, matrix2)
+	def multiply(matrix)
 		#Pre 
 		
 		#Post
-		
+		# this sparse matrix has been multiplied by given matrix 
 	end
 	
-	def divide(matrix1, matrix2)
+	def divide(matrix)
 		#Pre 
 		
 		#Post
-		
+		# this sparse matrix has been divided by given matrix
 	end
 	
-	def exponent(matrix1, matrix2)
+	def exponent(matrix)
 		#Pre 
 		
 		#Post
-		
+		# this sparse matrix has been set to the exponent of given matrix (???)
 	end
 	
 	def getDeterminant()
 		#Pre 
 		
 		#Post
-		
+		# none, determinant is returned
 	end
 	
 	def getInverse()
 		#Pre 
 		
 		#Post
-		
+		# matrix is set to its own inverse
 	end
 	
 	def getTranspose()
 		#Pre 
 		
 		#Post
-		
+		# matrix is set to its own transpose
 	end
 	
 	
