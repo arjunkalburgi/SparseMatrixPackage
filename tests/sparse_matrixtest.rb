88e4c109679c0662697157c73daa82ecf98e6a66
Andrew McKernan <amckerna@ualberta.ca>
2018-01-22 00:02:10 -0700

    some tests pass... :)

--- tests/sparse_matrixtest.rb
+++ tests/sparse_matrixtest.rb
@@ -1,18 +1,18 @@
 require 'test/unit'
 require_relative '../sparsematrixpackage/sparse_matrix.rb'
 
-class sparsematrixtest < Test::Unit::TestCase
-    def setup
-        pass 
-    end 
+class SparseMatrixTest < Test::Unit::TestCase
+    #def setup
+    #    pass 
+    #end 
 
-    def teardown
-        pass
-    end
+    #def teardown
+    #    pass
+    #end
 
-    def test_fail
-        assert(false, 'Assertion was false.')
-    end
+    #def test_fail
+    #    assert(false, 'Assertion was false.')
+    #end
 	
 	def test_add
 		matrix1 = SparseMatrix.new([0, 0, 1, 0])
