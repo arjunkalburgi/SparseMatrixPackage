1086a780ab8cbc2721e0b48c17c3923cbdd4b746
Bianca Angotti <bangotti@ualberta.ca>
2018-01-21 22:06:52 -0700

    Update tridiagonal_matrix.rb

--- sparsematrixpackage/tridiagonal_matrix.rb
+++ sparsematrixpackage/tridiagonal_matrix.rb
@@ -1,15 +1,32 @@
 # :)
 require 'matrix'
 
-class TriagonalMatrix
+# inherits from Matrix for utilization of 
+class TriDiagonalMatrix < Matrix
 	
-	def initialize(top, middle, bottom)
+	# let matrix handle these functions
+	# delegate [:+, :**, :-, :hermitian?, :normal?, :permutation?] => :Matrix.send(:new, to_a)
+	
+	# all public methods...
+	 
+	def initialize(rows)
+		#PRE
+		#ensure nxn
+		#ensure there are 3 diagonals of proper sizes
+		
+		rows = convert_to_array(row, true) # not sure if true is needed to copy object
+		
+		@size = (rows[0] || []).size 
+		
 		@top_diag = top
 		@middle_diag = middle
 		@bottom_diag = bottom
 		
+		#POST
 	end
 	
+	# all private methods...
 	
-
+	private 
+	
 end
