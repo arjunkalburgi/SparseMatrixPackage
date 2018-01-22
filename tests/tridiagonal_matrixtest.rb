d181da04a14a0c0581baa736e252aabddaef527f
Andrew McKernan <andrewmck96@gmail.com>
2018-01-21 23:39:33 -0700

    beginnings of tridiagonal_matrix tests

--- tests/tridiagonal_matrixtest.rb
+++ tests/tridiagonal_matrixtest.rb
@@ -13,4 +13,23 @@ class tridiagonalmatrixtest < Test::Unit::TestCase
     def test_fail
         assert(false, 'Assertion was false.')
     end
+	
+	def test_create
+		tridiagonal = TriDiagonalMatrix.new([[1,2,0,0,0],
+											 [3,1,2,0,0],
+										 	 [0,3,1,2,0],
+											 [0,0,3,1,2],
+											 [0,0,0,3,1])
+		# success?
+		assert(true, "create was successful")
+	end
+	
+	def test_bad_create
+		tridiagonal = TriDiagonalMatrix.new([[1,2,0,0,5],
+											 [0,1,2,0,0],
+										 	 [0,3,0,0,0],
+											 [0,0,3,1,2],
+											 [5,0,0,3,1])
+		# how to pass test on failure?
+	end
 end
