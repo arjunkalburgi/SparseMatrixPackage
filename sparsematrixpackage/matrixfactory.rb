11e4047de194539ad4d5834ee09d8341b4546ff5
Arjun Kalburgi <askalburgi@gmail.com>
2018-01-21 21:57:06 -0700

    factory and create

--- sparsematrixpackage/matrixfactory.rb
+++ sparsematrixpackage/matrixfactory.rb
@@ -1,8 +1,19 @@
+require 'tridagonal_matrix'
+require 'sparse_matrix'
 
 module MatrixFactory 
 
-    def createMatrix
-        
+    def self.create(classname, matrixarray)
+
+        case classname
+        when :tridiagonal
+            puts "hi tri"
+            TriagonalMatrix.new(matrixarray)
+        when :sparse 
+            puts "hi spar"
+            SparseMatrix.new(matrixarray)
+        end
+
     end
     
 end
