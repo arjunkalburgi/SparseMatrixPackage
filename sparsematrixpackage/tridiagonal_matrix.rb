beffdc5e75347355ccdb0c4f8ba48d402af6cd8f
Bianca Angotti <bangotti@ualberta.ca>
2018-01-23 23:55:54 -0700

    Update tridiagonal_matrix.rb

--- sparsematrixpackage/tridiagonal_matrix.rb
+++ sparsematrixpackage/tridiagonal_matrix.rb
@@ -1,4 +1,4 @@
-# :)
+
 require 'matrix'
 
 # inherits from Matrix for utilization of 
@@ -88,6 +88,60 @@ class TriDiagonalMatrix < Matrix
 			@lower_diagonal.eql?(other_object.get_lower_diagonal)
 	end
 	
+	def +(other_matrix)
+		#Pre 
+		# matrix is tridiagonal
+		begin 
+			raise "Other object is not tridiagonal" unless TriDiagonalMatrix === other_matrix
+		end 
+		# same dimensions
+		begin 
+			raise "Matricies do not have the same dimentions" unless @middle_diagonal.size == other_matrix.get_middle_diagonal.size
+		end
+
+		upper = [@upper_diagonal, other_matrix.get_upper_diagonal].transpose.map {|x| x.reduce(:+)}
+		middle = [@middle_diagonal, other_matrix.get_middle_diagonal].transpose.map {|x| x.reduce(:+)}
+		lower = [@lower_diagonal, other_matrix.get_lower_diagonal].transpose.map {|x| x.reduce(:+)}
+		
+		#Post
+		# this sparse matrix has had the contents of give matrix added to it
+		#new Tridiagonal???
+	end
+	
+	def -(other_matrix)
+		#Pre 
+		# matrix is tridiagonal
+		begin 
+			raise "Other object is not tridiagonal" unless TriDiagonalMatrix === other_matrix
+		end 
+		# same dimensions
+		begin 
+			raise "Matricies do not have the same dimentions" 
+				unless @middle_diagonal.size == other_matrix.get_middle_diagonal.size
+		end
+		
+		upper = [@upper_diagonal, other_matrix.get_upper_diagonal].transpose.map {|x| x.reduce(:-)}
+		middle = [@middle_diagonal, other_matrix.get_middle_diagonal].transpose.map {|x| x.reduce(:-)}
+		lower = [@lower_diagonal, other_matrix.get_lower_diagonal].transpose.map {|x| x.reduce(:-)}
+		
+		#Post
+		# this sparse matrix has had the contents of given matrix subtracted from it
+	end
+	
+	def *(matrix)
+		#Pre 
+		
+		#Post
+		# this sparse matrix has been multiplied by given matrix 
+	end
+	
+	def /(matrix)
+		#Pre 
+		
+		#Post
+		# this sparse matrix has been divided by given matrix
+	end
+	
 	def /(mat)
 		if mat.respond_to?("inverse")
 			self * mat.inverse
@@ -95,9 +149,44 @@ class TriDiagonalMatrix < Matrix
 			map {|x| x/mat}	
 		end
 	end
+		
+	def determinant
+# 		@middle_diagonal[1..-1].zip(@upper_diagonal, @lower_diagonal).reduce([1, @middle_diagonal[0]]) do |c, x|
+# 			c << x[0] * c.last - x[1] * x[2] * c[-2]
+# 		end.last
+	end
+		
+# 	def transpose!
+# 		@upper_diagonal, @lower_diagonal = @lower_diagonal, @upper_diagonal
+# 		self
+# 	end
+
+# 	def transpose
+# 		self.class.send(:new, @lower_diagonal, @middle_diagonal, @upper_diagonal)
+# 	end
 	
-	def map
-		to_enum :map
+	def square?
+		true
+	end
+
+# 	def diagonal?
+# 		@upper_diagonal.all? { |x| x == 0 } && @lower_diagonal.all? { |x| x == 0 }
+# 	end
+
+# 	def toeplitz?
+# 		@upper_diagonal.reduce(true) { |a, e| a && e == @upper_diagonal[0] }
+# 	end
+
+	def upper_triangular?
+		false
+	end
+
+	def orthogonal?
+		transpose == inverse
+	end
+
+	def symmetric?
+		self == transpose
 	end
 
 	# all private methods
