301d43e0ede3acdb2a90635594f27612aab98d99
Arjun Kalburgi <askalburgi@gmail.com>
2018-01-29 21:31:09 -0700

    refactoring presandposts

--- sparsematrixpackage/sparse_matrix.rb
+++ sparsematrixpackage/sparse_matrix.rb
@@ -7,7 +7,6 @@ class SparseMatrix
 
 	def initialize(matrixarray)
 		#Pre 
-
 		@num_rows = matrixarray.size
 		@num_columns = matrixarray[0].size
 
@@ -31,79 +30,56 @@ class SparseMatrix
 	end
 	
 	def +(othermatrix)
-		begin
-			raise "Cannot perform operation, deminsions do not match." unless @num_rows == othermatrix.num_rows && @num_columns == othermatrix.num_columns
-		end
-
-
-		matrix = othermatrix.matrix_table
+		check_matching_dimensions(othermatrix)
 		
-		all_keys_with_value = @matrix_table.keys + matrix.keys
-
-		all_keys_with_value.each do |key| 
-			@matrix_table[key] += matrix[key]
-		end
-
+		resultmatrix = multiply(othermatrix)
+		# matrix = othermatrix.matrix_table
 		
-		begin
-			raise "Matrix dimensions must remain the same." unless othermatrix.num_rows == (self+matrix).num_rows && othermatrix.num_columns == (self+matrix).num_columns
-		end
+		# all_keys_with_value = @matrix_table.keys + matrix.keys
+
+		# all_keys_with_value.each do |key| 
+		# 	@matrix_table[key] += matrix[key]
+		# end
+
+		check_dimensions_are_the_same(othermatrix, resultmatrix)
 	end
 	
 	def -(othermatrix)
-		begin
-			raise "Cannot perform operation, deminsions do not match." unless @num_rows == othermatrix.num_rows && @num_columns == othermatrix.num_columns
-		end
+		check_matching_dimensions(othermatrix)
 		
+		resultmatrix = subtraction(othermatrix)
+		# matrix = othermatrix.matrix_table
+		# all_keys_with_value = @matrix_table.keys + matrix.keys
 
-		matrix = othermatrix.matrix_table
-		all_keys_with_value = @matrix_table.keys + matrix.keys
+		# all_keys_with_value.each do |key| 
+		# 	@matrix_table[key] -= matrix[key]
+		# end
 
-		all_keys_with_value.each do |key| 
-			@matrix_table[key] -= matrix[key]
-		end
-
-
-		begin
-			raise "Matrix dimensions must remain the same." unless othermatrix.num_rows == (self-matrix).num_rows && othermatrix.num_columns == (self-matrix).num_columns
-		end
+		check_dimensions_are_the_same(othermatrix, resultmatrix)
 	end
 	
 	def *(othermatrix)
-		begin
-			raise "Cannot perform operation, deminsions are not compatible." unless @num_columns == othermatrix.num_rows
-			original_rows = @num_rows
-			orignal2_columns = othermatrix.num_columns
-		end
+		check_compatible_dimensions_for_multiplication(othermatrix)
 		
+		resultmatrix = multiplication(othermatrix)
 
-		begin
-			raise "Multiplication dimensions are incorrect." unless original_rows == (self*matrix).num_rows && orignal2_columns == (self*matrix).num_columns
-		end
+		check_correct_dimensions_after_multiplication(othermatrix, resultmatrix)
 	end
 	
 	def /(othermatrix)
-		begin
-			raise "Cannot perform operation, deminsions are not compatible." unless @num_columns == matrix.getInverse().num_rows
-			original_rows = @num_rows
-			orignal2_columns = matrix.getInverse().num_columns
-		end
+		check_compatible_dimensions_for_multiplication(othermatrix.getInverse())
 
-		self * matrix.getInverse()
-		
-		begin
-			raise "Multiplication dimensions are incorrect." unless original_rows == (self/matrix).num_rows && orignal2_columns == (self/matrix).num_columns
-		end
+		resultmatrix = multiplication(othermatrix.getInverse())
+
+		check_correct_dimensions_after_multiplication(othermatrix.getInverse())
 	end
 	
 	def getDeterminant()
-		begin
-			raise "Cannot perform operation, dimensions are not compatible." unless @num_columns == @num_rows
-		end
+		check_square_matrix()
 		
-		begin
-			raise "Determinant is a number" unless self.getDeterminant().is_a? Numeric
-		end
+		resultmatrix = getMatrixDeterminant()
+
+		
 	end
 	
 	def getInverse()
@@ -139,6 +115,42 @@ class SparseMatrix
 		begin
 		end
 	end
+
+	# real?, imaginary?, real(), imaginary()
+
+
 	
+	private
+
+	def check_matching_dimensions(othermatrix)
+		begin
+			raise "Cannot perform operation, deminsions do not match." unless @num_rows == othermatrix.num_rows && @num_columns == othermatrix.num_columns
+		end
+	end
+
+	def check_dimensions_are_the_same(othermatrix, result)
+		begin
+			raise "Matrix dimensions must remain the same." unless othermatrix.num_rows == result.num_rows && othermatrix.num_columns == result.num_columns
+		end
+	end
+
+	def check_compatible_dimensions_for_multiplication(othermatrix) 
+		begin
+			raise "Cannot perform operation, deminsions are not compatible." unless @num_columns == othermatrix.num_rows
+		end
+		# {row: @num_rows, column: othermatrix.num_columns}
+	end
+
+	def check_correct_dimensions_after_multiplication(othermatrix, result)
+		begin
+			raise "Multiplication dimensions are incorrect." unless @num_rows == result.num_rows && @num_columns == result.num_columns
+		end
+	end
+
+	def check_square_matrix()
+		begin
+			raise "Cannot perform operation, dimensions are not compatible." unless @num_columns == @num_rows
+		end
+	end
 end
 
