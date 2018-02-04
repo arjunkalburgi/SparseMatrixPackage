fd5dfa73cda91e5f3887b11357a246465b213221
Arjun Kalburgi <askalburgi@gmail.com>
2018-02-04 14:18:29 -0700

    removing commented out tests that were commented out for testing lol

--- sparsematrixpackage/sparse_matrix.rb
+++ sparsematrixpackage/sparse_matrix.rb
@@ -70,11 +70,11 @@ class SparseMatrix
 		result_matrix = nil
 		case other
 			when SparseMatrix
-				# check_compatible_dimensions_for_multiplication(other)
+				check_compatible_dimensions_for_multiplication(other)
 				
 				result_matrix = multiplication(other)
 
-				# check_correct_dimensions_after_multiplication(other, result_matrix)
+				check_correct_dimensions_after_multiplication(other, result_matrix)
 				
 			else 
 				result_matrix = self * (new other)
