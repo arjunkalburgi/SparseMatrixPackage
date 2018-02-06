c19fccd18c5fdff654c4c7bc213502d5d1b7be6d
Arjun Kalburgi <askalburgi@gmail.com>
2018-02-06 16:24:34 -0700

    ass

--- assignment1_main.rb
+++ assignment1_main.rb
@@ -1,7 +1,7 @@
 # Project 1
 # Main file 
 
-# See test files for execution instructions 
+# Functionality of the sparse matrix package
 
 # Group: Winter2018_Group1
 #     Bianca Angotti 
@@ -10,12 +10,58 @@
 
 require_relative 'sparsematrixpackage'
 
-# All functionality 
+# MatrixFactory builds Sparse and Tridiagonal matrices
+sparseMatrix = MatrixFactory.create(SparseMatrix, [[1, 0, 0], 
+                                                   [0, 4, 0],
+                                                   [0, 1, 0]])
+triDiagonal = MatrixFactory.create(TridiagonalMatrix, [[1, 0, 0],
+                                                       [0, 1, 0],
+                                                       [0, 0, 1]])
 
-# Plan to utilize factory and call different matricies
-sparseMatrix = MatrixFactory.create(SparseMatrix, [[1, 0, 0], [0, 4, 0], [0, 1, 0]])
-triDiagonal = MatrixFactory.create(TridiagonalMatrix, [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 
-# test operations
-sparseMatrix - triDiagonal
-# add, divide, det, etc
+
+# Benchmarks
+require 'test/unit'
+require_relative './tests/sparse_matrixtest'
+require_relative './tests/tridiagonal_matrixtest'
+
+
+
+# Math Operations 
+triDiagonal + sparse
+
+sparse - triDiagonal
+
+sparse * 2
+
+triDiagonal * sparse
+
+sparse / 2
+
+triDiagonal.determinant
+
+triDiagonal.inverse
+
+sparse.transpose
+
+
+
+# Properties
+triDiagonal.diagonal?
+
+sparse.zero?
+
+sparse.square?
+
+triDiagonal.real?
+
+sparse.imaginary?
+
+triDiagonal.orthoganal?
+
+
+
+# Enumerate
+sparse.each(:nonzero) { |x| x }
+
+triDiagonal.each(:tridiagonal) { |x| x }
